4f1a72b585f53da3b8a2cfb643c51fb95c03ffdc
kirfed <kirfed@gmail.com>
2020-01-10 09:03:12 +0300

    hex radiuses

--- src/hex.rs
+++ src/hex.rs
@@ -162,6 +162,14 @@ impl Hex {
             self.left(),
         ]
     }
+
+    pub fn inner_radius(&self) -> f32 {
+        self.size.1 / 2f32
+    }
+
+    pub fn outer_radius(&self) -> f32 {
+        self.size.0 / 2f32
+    }
 }
 
 #[cfg(test)]
